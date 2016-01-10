c6e9185ecb07cbb0148046eab0f5223cd60649b4
donte <digitalartcrew@gmail.com>
2016-01-09 19:07:46 -0800

    Facebook setup, need to whitelist urls

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,6 +39,78 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
+
+  $scope.signupEmail = function(){  
+ 
+  var ref = new Firebase("https://shining-fire-8120.firebaseio.com");
+ 
+  ref.createUser({
+    email    : $scope.data.email,
+    password : $scope.data.password
+  }, function(error, userData) {
+    if (error) {
+      console.log("Error creating user:", error);
+    } else {
+      console.log("Successfully created user account with uid:", userData.uid);
+    }
+  });
+ 
+};
+
+$scope.loginEmail = function(){
+ 
+  var ref = new Firebase("https://shining-fire-8120.firebaseio.com");
+ 
+  ref.authWithPassword({
+    email    : $scope.data.email,
+    password : $scope.data.password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+ 
+};
+
+$scope.loginFacebook = function(){
+ 
+  var ref = new Firebase("https://shining-fire-8120.firebaseio.com");
+ 
+ 
+  if(ionic.Platform.isWebView()){
+ 
+    $cordovaFacebook.login(["public_profile", "email"]).then(function(success){
+ 
+      console.log(success);
+ 
+      ref.authWithOAuthToken("facebook", success.authResponse.accessToken, function(error, authData) {
+        if (error) {
+          console.log('Firebase login failed!', error);
+        } else {
+          console.log('Authenticated successfully with payload:', authData);
+        }
+      });
+ 
+    }, function(error){
+      console.log(error);
+    });        
+ 
+  }
+  else {
+ 
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+ 
+  }
+ 
+};
 })
 
 
