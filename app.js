34c982007b784dc636a2fa3ec7587c9e833d32ca
donte <digitalartcrew@gmail.com>
2016-01-15 14:53:42 -0800

    4 Square API

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(cors());
 var url = 'http://ufc-data-api.ufc.com/api/v1/us/';
 var fs_api ='https://api.foursquare.com/v2/venues/search?client_id='+process.env.FS_CLIENTID+'&client_secret='+process.env.FS_CLIENTSECRET+'&v=20130815&ll=40.7,-74&query=';
 
+//UFC API Starts Here
 app.get('/news', function(req,res){
 	request(url+'news', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
@@ -47,50 +48,54 @@ app.get('/media', function(req,res){
 	});
 });
 
+//UFC API Ends Here
+
+
+//Foursquare API Starts Here
 app.get('/mma', function(req,res){
-	request('fs_api'+mma, function(error, response, body) {
+	request(fs_api+'mma', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
 });
 
 app.get('/boxing', function(req,res){
-	request('fs_api'+boxing, function(error, response, body) {
+	request(fs_api+'boxing', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
 });
 
 app.get('/bjj', function(req,res){
-	request('fs_api'+bjj, function(error, response, body) {
+	request(fs_api+'bjj', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
 });
 
 app.get('/muaythai', function(req,res){
-	request('fs_api'+muay&thai, function(error, response, body) {
+	request(fs_api+'muay&thai', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
 });
 
 app.get('/wrestling', function(req,res){
-	request('fs_api'+wrestling, function(error, response, body) {
+	request(fs_api+'wrestling', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
 });
 
 app.get('/yoga', function(req,res){
-	request('fs_api'+yoga, function(error, response, body) {
+	request(fs_api+'yoga', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
 });
 
 app.get('/fitness', function(req,res){
-	request('fs_api'+fitness&gym, function(error, response, body) {
+	request(fs_api+'fitness&gym', function(error, response, body) {
 	  if (error || response.statusCode !== 200) return res.status(404).json({error: error});
 	  res.status(200).json(body);
 	});
