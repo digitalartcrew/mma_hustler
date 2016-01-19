6af4c56a9104026e61cadbcd8eee7dc1cf1e5da7
donte <digitalartcrew@gmail.com>
2016-01-19 11:43:20 -0800

    Updated facebook login for ios

--- www/js/app.js
+++ www/js/app.js
@@ -4,13 +4,16 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers','firebase','ngCordova','youtube-embed'])
+var app = angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers','firebase','ngCordova','youtube-embed','openfb'])
 
 .constant('ApiEndpoint', {
   url: 'http://localhost:8100/api'
 })
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$rootScope, $state,$window, OpenFB) {
+
+  OpenFB.init('1679579365644407');
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -29,7 +32,20 @@ app.run(function(MyDataService) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+
+    $rootScope.$on('$stateChangeStart', function(event, toState) {
+            if (toState.name !== "app.login" && toState.name !== "app.logout" && !$window.sessionStorage.fbtoken) {
+                $state.go('app.login');
+                event.preventDefault();
+            }
+        });
+
+        $rootScope.$on('OAuthException', function() {
+            $state.go('app.login');
+        });
   });
+
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
