f10ff66dc5a561817adb4ca19f1aee2896c5a99e
donte <digitalartcrew@gmail.com>
2016-01-22 13:40:12 -0800

    Formatted Date

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3439,7 +3439,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"c5014da7","api_key":"2db5b2e8260f4067887f4e901306dc798f3680dcde6df864","dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"c781b8ae","api_key":"2bc800325c97c4ae4d8dee6467eaadf3e011b6bdcdf539a4","dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {
